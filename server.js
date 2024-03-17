8969853acfa7045fbe83930263b830777abe4c9a
rajput999 <rajput9997647419@gmail.com>
2024-03-17 23:01:41 +0530

    google oath2

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const session = require('express-session');
 const cors = require("cors");
+require('./validation/auth.js')
 
 const users = require('./routes/api/users');
 // const profile = require('./routes/api/profile');
@@ -14,7 +16,19 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+function isLoggedIn(req,res,next){
+    req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure:false}
+  }));
+
 const db = require('./config/keys').mongoURI;
+
 
 mongoose
     .connect(db)
@@ -22,7 +36,37 @@ mongoose
     .catch(err => console.log(err));
 
 app.use(passport.initialize());
+app.use(passport.session());
 require('./config/passport')(passport);
+
+
+
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/protected',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/failure',(req,res)=>{
+    res.send("Something went wrong");
+});
+
+app.get('/auth/protected',isLoggedIn,(req,res)=>{
+    let name = req.user;
+    
+    res.send(`hello ${name.displayName}`);
+});
+
+
+
+
 
 app.use('/api/users', users);
 // app.use('/api/profile', profile);
