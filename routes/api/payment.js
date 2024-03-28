fe58a7011b3d35fc2e9c44a6d193eb4196ed5f66
shrinish123 <shrinishiitkgp2001@gmail.com>
2024-03-29 04:37:11 +0530

    payment

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -2,6 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const Subscription = require("../../models/Subscription");
 
 router.post("/checkout",auth, async (req, res) => {
   const {amt,name } = req.body;
@@ -11,7 +12,7 @@ router.post("/checkout",auth, async (req, res) => {
         line_items: [
           {
             price_data:{
-              currency:"usd",
+              currency:"inr",
               product_data:{
                 name:name
               },
@@ -22,14 +23,38 @@ router.post("/checkout",auth, async (req, res) => {
         ],
         mode: 'payment',
         payment_method_types:["card"],
-        success_url: 'http://localhost:5173/plans',
-        cancel_url: 'http://localhost:5173/plans',
+        success_url: 'http://localhost:5173',
+        cancel_url: 'http://localhost:5173',
       });
 
     res.status(200).json({id:session.id});
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+
+router.post('/webhook', async (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+    
+    console.log('Payment completed:', session);
+    
+
+  }
+
+  res.json({ received: true });
 });
 
 
